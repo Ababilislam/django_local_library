d751fab8ae45114629033bb696b06424319b12ac
ABABIL ISLAM UDOY <ababilislamudoy@gmail.com>
2024-02-27 21:38:16 +0600

    setup model for apps

--- django_local_library/urls.py
+++ django_local_library/urls.py
@@ -17,14 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+# from catalog import urls as catalog_urls
 
 from django_local_library import settings
 from django.conf.urls.static import static
 
 urlpatterns = ([
     path('admin/', admin.site.urls),
-    path('catalog/', include('catalog.urls', namespace='catalog_url')),
-    path("", RedirectView.as_view(url='/catalog/')),
+    path('catalog/', include('catalog.urls')),
+    # path("", RedirectView.as_view(url='/catalog/')),
 
 ]
 )
